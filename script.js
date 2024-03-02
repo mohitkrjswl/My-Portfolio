90e92c96d9193ef065ee032d0d19aa05391722d2
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-03-02 15:47:33 +0530

    f

--- script.js
+++ script.js
@@ -32,3 +32,26 @@ window.onscroll = () => {
 //  remove toggle and navbar-------
 menuIcon.classList.remove("fa-xmark");
 navbar.classList.remove("active");
+// scroll show-----------
+
+ScrollReveal({
+  distance: "8-px",
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".homeimage, .service-container,.portfolio-box,contact form",
+  { origin: "buttom" }
+);
+ScrollReveal().reveal(".home-contact h1", { origin: "left" });
+ScrollReveal().reveal(".home-contact p,.about-content", { origin: "right" });
+// typed js------
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
